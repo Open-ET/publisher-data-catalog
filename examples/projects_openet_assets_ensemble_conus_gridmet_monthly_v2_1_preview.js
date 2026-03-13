c93f9b232c229331ff097ce9218226533823453e
OpenET <admin@openetdata.org>
2026-03-13 16:19:03 -0700

    Changed preview example image for 2.1

--- examples/projects_openet_assets_ensemble_conus_gridmet_monthly_v2_1_preview.js
+++ examples/projects_openet_assets_ensemble_conus_gridmet_monthly_v2_1_preview.js
@@ -7,7 +7,7 @@ var et = dataset.select('et_ensemble_mad').sum();
 
 var visParams = {
   min: 0,
-  max: 1400,
+  max: 1000,
   palette: [
     '9e6212', 'ac7d1d', 'ba9829', 'c8b434', 'd6cf40', 'bed44b', '9fcb51',
     '80c256', '61b95c', '42b062', '45b677', '49bc8d', '4dc2a2', '51c8b8',
@@ -18,13 +18,13 @@ var visParams = {
 var rgbImage = et.visualize(visParams);
 Map.addLayer(et, {}, 'OpenET Ensemble Annual ET', true, 0.3);
 
-var lon = -120.68758;
-var lat = 36.75228;
-Map.setCenter(lon, lat, 11);
+var lon = -116.47;
+var lat = 40.83;
+Map.setCenter(lon, lat, 10);
 
 // Degrees in EPSG:3857
-var lonDelta = 0.045;
-var latDelta = 0.035;
+var lonDelta = 0.1;
+var latDelta = 0.085;
 // Width and height of the thumbnail image.
 var pixels = 256;
 
